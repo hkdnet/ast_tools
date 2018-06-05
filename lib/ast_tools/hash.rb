973ec313be45948dbad102f052c5a4a46f0dc83f
hkdnet <satoko.itse@gmail.com>
2018-06-05 14:59:49 +0900

    Use AstTools::Hash.convert

--- lib/ast_tools/hash.rb
+++ lib/ast_tools/hash.rb
@@ -1,10 +1,14 @@
 module AstTools
   module Hash
+    def self.convert(nodes)
+      nodes.each_with_object({}) do |e, h|
+        h[e.type.downcase] = e if e
+      end
+    end
+
     refine RubyVM::AST::Node do
       def children
-        super.each_with_object({}) do |e, h|
-          h[e.type.downcase] = e if e
-        end
+        ::AstTools::Hash.convert(super)
       end
     end
   end
